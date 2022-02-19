0cb8f626f5d9f055501ed89bfb17f71b27af02fe
jhegner <jidlafehegner@gmail.com>
2022-02-19 08:47:05 -0300

    mensagem simples com proto

---
+++ src/main/java/br/com/jhegnerlabs/proto/SimpleMessageProtoMain.java
@@ -0,0 +1,9 @@
+package br.com.jhegnerlabs.proto;
+
+public class SimpleMessageProtoMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Proto !!!");
+    }
+
+}
